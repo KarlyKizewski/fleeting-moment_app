a7383b20d85e1b46acf0427d9c011c4403452db1
Karly Kizewski <kizewski.karly@gmail.com>
2019-08-28 19:52:16 +0000

    failed test for create post action because there is no controller action

--- spec/controllers/moments_controller_spec.rb
+++ spec/controllers/moments_controller_spec.rb
@@ -14,4 +14,14 @@ RSpec.describe MomentsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
   end
+
+  describe "moments#create action" do
+    it "should successfully create a moment in our database" do
+      post :create, params: { moment: { message: 'Hello!' } }
+      expect(response).to redirect_to root_path
+
+      moment = Moment.last
+      expect(moment.message).to eq("Hello!")
+    end
+  end
 end
