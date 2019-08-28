83da5bee4f7b53e7aaea1604798a872d32c92821
Karly Kizewski <kizewski.karly@gmail.com>
2019-08-28 19:58:28 +0000

    test success for create action post to database

--- app/controllers/moments_controller.rb
+++ app/controllers/moments_controller.rb
@@ -6,4 +6,15 @@ class MomentsController < ApplicationController
 
   def index
   end
+
+  def create
+    @moment = Moment.create(moment_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def moment_params
+    params.require(:moment).permit(:message)
+  end
 end
